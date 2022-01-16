71c1fc83844cd6a1d70dd1856f7bc2f0993b4f8c
geno <gabrigeno@gmail.com>
2022-01-16 16:32:54 +0100

    commit di prova

--- phase1/asl.c
+++ phase1/asl.c
@@ -1,4 +1,10 @@
 #include "asl.h"
 
-
+/* array di SEMD con dimensione massima di MAXPROC */
+semd_PTR semd_table[MAXPROC];
 semdFree_t smdFree_h;
+
+
+int insertBlocked(int *semAdd, pcb_t *p) {
+    
+}
--- phase1/asl.h
+++ phase1/asl.h
@@ -4,23 +4,26 @@
 #include "pandos_const.h"
 #include "pandos_types.h"
 
-//array di SEMD con dimensione massima di MAXPROC
-semd_PTR semd_table[MAXPROC];
 
-
-//Lista dei SEMD liberi o inutilizzati
+/* Lista dei SEMD liberi o inutilizzati */
 typedef struct semdFree_t {
-
+    semd_t val;
+    semdFree_t *next;
+    semdFree_t *prev;
+    struct list_head sentry;
 } semdFree_t, *semdFree_PTR;
 
 
-//Lista dei semafori attivi ASL
+/* Lista dei semafori attivi ASL */
 typedef struct asl_t {
-
+    semd_t val;
+    asl_t *next;
+    asl_t *prev;
+    struct list_head sentry;
 } asl_t, *asl_PTR;
 
 
-//funzioni di gestione dell'ASL
+/* funzioni di gestione dell'ASL */
 
 /*
     Viene inserito il PCB puntato da p nella coda dei 
