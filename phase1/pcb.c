0178862ae11ed0809af9dbe8569c2e5bcf7b7861
kocierik <kocierik@gmail.com>
2022-02-10 16:50:13 +0100

    Co-authored-by: Paolo Ceroni <pazero@users.noreply.github.com>

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -21,8 +21,26 @@ pcb_t *allocPcb(){
 	if(list_empty(&pcbFree_h)) return NULL;
 	else {
 		pcb_t savedElement = &pcbFree_h->next;
-		list_del(savedElement);
-		LIST_HEAD_INIT(&pcbFree_h);
+		pcbFree_h->next = pcbFree_h->next->next;
+		savedElement.p_child = NULL;
+		savedElement.p_list = NULL;
+		savedElement.p_parent = NULL;
+		savedElement.p_semAdd = NULL;
+		savedElement.p_sib = NULL;
+		savedElement.p_time = 0;
 		return savedElement;
 	}
+}
+//???
+void mkEmptyProcQ(struct list_head *head){
+	pcb_t list = new pcb_t;
+	list.p_list = head;
+}
+
+int emptyProcQ(struct list_head *head){
+	return list_empty(&head) ? true : false;
+}
+
+void insertProcQ(struct list_head *head, pcb_t* p){
+	list_add_tail(&p,&head);
 }
