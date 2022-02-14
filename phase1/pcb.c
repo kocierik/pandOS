9fbb5ac6f849057cdceb54d00a7e2bbbf0d99e4d
Paolo Ceroni <paoloceroni01@gmail.com>
2022-02-14 17:38:38 +0100

    pcb.c da branch pcb

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -3,45 +3,32 @@
 
 //array di PCB con dimensione massima di MAXPROC
 pcb_t pcbFree_table[MAXPROC];
-struct list_head *pcbFree_h;
+//struct list_head *pcbFree_h;
+LIST_HEAD(pcbFree_h);
 
 void initPcbs(void){
-  //pcbFree_h = LIST_HEAD_INIT(pcbFree_h);
   for(int i=0; i < MAXPROC; i++){
 		pcb_t* pcb = &pcbFree_table[i];
 		list_add_tail(&pcb->p_list, &pcbFree_h);
 	}
 }
 
-void freePcb(pcb_t * p){
-	list_add(&p, &pcbFree_h);
+void freePcb(pcb_t *p){
+	list_add(&p->p_list, &pcbFree_h);
 }
-
-void *my_memset(void *s, int c,  unsigned int len) {
-    unsigned char* p=s;
-    while(len--)
-    {
-        *p++ = (unsigned char)c;
-    }
-    return s;
-}
-
 
 pcb_t *allocPcb(){
 	if(list_empty(&pcbFree_h)) return NULL;
 	else {
-		pcb_t* savedElement = pcbFree_h->next;
-		pcbFree_h->next = pcbFree_h->next->next;
+		pcb_t *savedElement = container_of(pcbFree_h.next,pcb_t,p_list);
+		pcbFree_h.next = pcbFree_h.next->next;
 
-		savedElement->p_list = savedElement->p_list;
+		INIT_LIST_HEAD(&savedElement->p_list);
 
-		savedElement->p_child = savedElement->p_child;
-		savedElement->p_parent = savedElement->p_parent;
+		savedElement->p_parent = NULL;
+		INIT_LIST_HEAD(&savedElement->p_child);
+		INIT_LIST_HEAD(&savedElement->p_sib);
 
-		savedElement->p_child = savedElement->p_child;
-		savedElement->p_sib = savedElement->p_sib;
-
-		memset(savedElement->p_s, 0, sizeof savedElement->p_s);
 		savedElement->p_time = 0;
 		savedElement->p_semAdd = NULL;
 		return savedElement;
@@ -49,33 +36,34 @@ pcb_t *allocPcb(){
 }
 
 void mkEmptyProcQ(struct list_head *head){
-	//head = LIST_HEAD_INIT(head);
+	INIT_LIST_HEAD(head);
 }
 
 int emptyProcQ(struct list_head *head){
-	return list_empty(&head) ? 1 : 0;
+	return list_empty(head) ? 1 : 0;
 }
 
 void insertProcQ(struct list_head* head, pcb_t* p){
-	list_add_tail(&p,&head);
+	list_add_tail(&p->p_list,head);
 }
-
+//forse buona
 pcb_t* headProcQ(struct list_head* head){
-	return list_empty(&head) ? NULL : head->next;
+	return list_empty(head) ? NULL : container_of(head->next,pcb_t,p_list);
 }
 
 pcb_t* removeProcQ(struct list_head* head){
-    if(list_empty(&head)) return NULL;
-    pcb_t* removedElement = head->next;
+    if(list_empty(head)) return NULL;
+    struct list_head* removedElement = head->next;
     head->next = head->next->next;
-    return removedElement;
+    return container_of(removedElement,pcb_t,p_list);
 }
-
+//forse buona
 pcb_t* outProcQ(struct list_head* head, pcb_t* p){
     struct list_head* iter;
     list_for_each(iter,head) {
-        if(strcmp(&iter,&p)==0){
-            list_del(&p);
+        if(container_of(iter,pcb_t,p_list) == p){
+            //list_del(&p->p_list);
+            list_del(iter);
             return p;
         }
     }
@@ -89,18 +77,26 @@ int emptyChild(pcb_t *p){
 }
 
 void insertChild(pcb_t *prnt, pcb_t *p){
-    list_add(&p,&prnt->p_child);
+    list_add(&p->p_list,&prnt->p_child);
 }
-
+//forse buono
 pcb_t* removeChild(pcb_t *p){
     if(list_empty(&p->p_child)) return NULL;
-    pcb_t* removedElement = p->p_child.next;
-    list_del(&(p->p_child.next));
-    return removedElement;
+    struct list_head *removedElement = p->p_child.next;
+    list_del(p->p_child.next);
+    return container_of(removedElement,pcb_t,p_child);
 }
 
 pcb_t* outChild(pcb_t *p){
-    if(list_empty(&p->p_parent)) return NULL;
-    list_del(&p);
-    return p;
+    if(p->p_parent != NULL) {
+        struct list_head* iter;
+        list_for_each(iter,&p->p_parent->p_child) {
+            if(container_of(iter,pcb_t,p_list) == p){
+                //list_del(&p->p_list);
+                list_del(iter);
+                return p;
+            }
+        }
+    }
+    return NULL;
 }
