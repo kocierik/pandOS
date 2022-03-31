3d7a471e3f81a6441913ecb47268adbdbf422ecc
Apo2203 <apollonio_francesco@libero.it>
2022-03-31 16:28:42 +0200

    Commenti e aggiunta timer

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -11,42 +11,50 @@ extern short semDevice[SEMDEVLEN];
 
 
 void scheduler() {
-    pcb_PTR p;
+    
+    pcb_PTR p; //Puntatore al processo che sto per prendere in carico. 
 
     // Scheduling dei processi ad alta priorita'
-    if((p = removeProcQ(&queueHighProc)) != NULL) {
+    /*
+    * Verifico che la coda dei processi ad alta priorità non sia vuota. Se non lo è estraggo il processo che è li ad attendere;
+    * e lo assegno ad una variabile indicante il processo correntemente attivo. 
+    */
+    if((p = removeProcQ(&queueHighProc)) != NULL) { 
         currentActiveProc = p;
         LDST(&p->p_s);
-    } else {
-
+    } else if ((p = removeProcQ(&queueLowProc)) != NULL) {
         // Scheduling dei processi a bassa priorita'
-        if ((p = removeProcQ(&queueLowProc)) != NULL) {
-            currentActiveProc = p;
-            //Load 5 milliseconds on the PLT.
-            LDST(&p->p_s);
-        } else {
-            // Se le code dei processi ready sono vuote, esegui i seguenti controlli
+        currentActiveProc = p;
+        //Load 5 milliseconds on the PLT.
+        setTIMER(5);
+        LDST(&p->p_s);
+    } else {
+        /*
+        * A questo punto se vi era un processo in una delle code (Alta o Bassa priorità) questo sarà stato passato
+        * dallo stato di "ready" allo stato "running".
+        * Altrimenti, se le code dei processi "ready" sono vuote, eseguo i seguenti controlli.       
+        */
+        
+        if(activeProc == 0)
+            HALT(); //Il processore non deve fare niente quindi si ferma
             
-            if(activeProc == 0)
-                HALT(); //Il processore non deve fare niente quindi si ferma
-                
-            if(activeProc > 0 && blockedProc > 0) {
-                /* TODO
-                IMPORTANT POINT
-                - set the Status register to enable interrupts
-                - disable the PLT (also through the Status register), or load it with a very large value
-                
-                The first interrupt that occurs after entering a Wait State should not be for the PLT.
-                */
-                //(memaddr) LOCALTIMERINT = 0xFFFFFFFF;
-                WAIT(); //twiddling its thumbs
-            }
-
-            if(activeProc > 0 && blockedProc == 0) {
-                /* Take an appropriate deadlock detected action;   -> che minchia vuol dire questa cosa
-                   invoke the PANIC BIOS service/instruction.  */
-                PANIC();        //DEADLOCK
-            }
+        if(activeProc > 0 && blockedProc > 0) {
+            /* TODO
+            IMPORTANT POINT
+            - set the Status register to enable interrupts
+            - disable the PLT (also through the Status register), or load it with a very large value
+            
+            The first interrupt that occurs after entering a Wait State should not be for the PLT.
+            */
+            //(memaddr) LOCALTIMERINT = 0xFFFFFFFF;
+            WAIT(); //twiddling its thumbs
         }
+
+        if(activeProc > 0 && blockedProc == 0) {
+            /* Take an appropriate deadlock detected action;   -> che minchia vuol dire questa cosa
+                invoke the PANIC BIOS service/instruction.  */
+            PANIC();        //DEADLOCK
+        }
+        
     }
 }
