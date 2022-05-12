23ae9f0bade173922c8a9c996e6508ef426fb528
Apo2203 <apollonio_francesco@libero.it>
2022-05-12 12:24:32 +0200

    inizializzazione

---
+++ phase3/p3test.c
@@ -0,0 +1,18 @@
+#include "../generic_headers/pandos_const.h"
+#include "../generic_headers/pandos_types.h"
+#include <umps/libumps.h>
+
+
+/*
+La funzione test dovra’:
+- Inizializzare le strutture dati di fase 3
+- Caricare e far partire l’esecuzione dei
+processi che vi forniamo
+- Mettersi in attesa della fine di questi ultimi
+(e’ opportuno che il sistema si fermi una
+volta terminati tutti)
+*/
+
+void test() {
+
+}
